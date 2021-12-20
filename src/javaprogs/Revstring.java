5a2ff82a29c5af4343cfa4d1e8ae3e10448e4b97
Beetroot2314 <amberkumar123827@gmail.com>
2021-12-20 16:09:05 +0530

    Third Commit

--- src/javaprogs/Revstring.java
+++ src/javaprogs/Revstring.java
@@ -1,28 +1,24 @@
 package javaprogs;
-import java.io.*;
 
+import java.io.*;
 
 public class Revstring {
 
-	public static void main(String args[])throws IOException
-	{
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		System.out.println("Enter a string to be reversed");
-		String str=br.readLine();
-		
-		String rev="";
-		
-		for(int i=0;i<str.length();i++)
-		{
-			char ch=str.charAt(i);
-			rev=ch+rev;
+		String str = br.readLine();
+
+		String rev = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			rev = ch + rev;
 		}
-		
-		System.out.println("Reverse of the string "+str+" is "+rev);
-		
-		
-		
+
+		System.out.println("Reverse of the string " + str + " is " + rev);
+
 	}
-	
+
 }
--- src/javaprogs/StringPalindrome.java
+++ src/javaprogs/StringPalindrome.java
@@ -1,29 +1,26 @@
 package javaprogs;
+
 import java.io.*;
 
 public class StringPalindrome {
-	
-	
-	public static void main(String args[])throws IOException
-	{
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter a string to be checked for Palindrome");
-		String str=br.readLine();
-		String rev="";
-		
-		for(int i=0;i<str.length();i++)
-		{
-			char ch1=str.charAt(i);
-			rev=ch1+rev;
-			
-				
+		String str = br.readLine();
+		String rev = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch1 = str.charAt(i);
+			rev = ch1 + rev;
+
 		}
-		
-		if(str.equals(rev))
+
+		if (str.equals(rev))
 			System.out.println("Palindromic String");
 		else
 			System.out.println("Non Palindromic String");
-		
+
 	}
 
 }
